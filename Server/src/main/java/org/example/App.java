48d293c4ec8e55985b9a42fcb5c515b92ad5e068
rus215 <Zby)(zby1998>
2021-03-07 13:37:23 +0500

    Added client package

--- Server/src/main/java/org/example/App.java
+++ Server/src/main/java/org/example/App.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 /**
- * Hello world!
- *
+ * gRPC server runner
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080).addService(new GreetingServiceImpl())
+                .build();
+
+        server.start();
+        server.awaitTermination();
     }
 }
